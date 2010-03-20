ee8ce8104a4222239e62f664ddc55d05679fd8e3
Scott Vokes <vokes.s@gmail.com>
2010-03-20 08:42:55 -0400

    Add makefile config files

---
+++ config.mk
@@ -0,0 +1,33 @@
+# Lua-specific paths and libraries
+LUA_VER=	5.1
+LUA=		/usr/local/bin/lua
+LUA_LIBPATH=	-L/usr/local/lib/
+LUA_LIBS=	-llua -lm
+LUA_INC=	-I/usr/local/include/
+LUA_FLAGS=	${LUA_INC} ${LUA_LIBPATH} ${LUA_LIBS}
+
+
+# Where compiled libraries and .lua sources install.
+LUA_DEST_LIB=	/usr/local/lib/lua/${LUA_VER}/
+LUA_DEST_LUA=	/usr/local/share/lua/${LUA_VER}/
+
+
+# Additional C settings
+CC=		cc
+LIB_PATHS=	#-L/usr/X11R6/lib/
+LIBS=		# -lX11
+INC=		-lev
+CFLAGS=		-Wall -shared -fPIC
+LIBEXT=		.so
+
+
+# Other tools, optional
+LINT=		lint
+ARCHNAME= 	lua-${LIBNAME}
+TESTSUITE=	test.lua
+
+
+# Build targets
+LIBNAME=	evc
+LIBPREFIX=	
+INST_LIB=	${LIBNAME}${LIBEXT}
