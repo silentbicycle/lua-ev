7599b44101f8570346fe9d33df57dd929880f492
Scott Vokes <vokes.s@gmail.com>
2010-05-09 10:24:18 -0400

    Add test-periodic.lua. (periodic is like timer, but based on wall clock.)

---
+++ test-periodic.lua
@@ -0,0 +1,20 @@
+require "evc"
+
+local loop = evc.default_loop()
+print("loop: ", loop)
+
+local p = evc.periodic_init(2, 0.5)
+print("periodic: ", p)
+
+local ct = 0
+p:set_cb(function(w, ev)
+            print("Tick", w, evc.time())
+            ct = ct + 1
+            if ct == 5 then p:stop(loop) end
+         end)
+p:start(loop)
+
+print "starting loop"
+loop:loop()
+
+print "done"
